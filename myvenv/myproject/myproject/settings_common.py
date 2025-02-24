bb0147afdee2cc1380191c2698ab1df031721f68
tyan-man <ringo.ri.1208@gmail.com>
2025-02-24 12:18:31 +0900

    設定ファイル修正

--- myvenv/myproject/myproject/settings_common.py
+++ myvenv/myproject/myproject/settings_common.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "myproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -138,7 +138,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
--- myvenv/myproject/myproject/urls.py
+++ myvenv/myproject/myproject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("crud.urls")),
-    path("accounts/", include("allauth.urls")),
+    path('', include('crud.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
